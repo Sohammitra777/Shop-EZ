31f157eb4507f437ec7d5a1e3ea245ddf36dda66
Aquib younis <aquibyounis1@gmail.comgit config --global user.email aquibyounis1@gmail.com>
2024-07-03 23:19:11 +0530

    product page added

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { HomeContext } from '../Context/HomeContext'
+import { useParams } from 'react-router-dom';
+import BreadCrum from '../components/BreadCrums/BreadCrum';
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const{all_products}=useContext(HomeContext);
+  const {productId}=useParams();
+  const product=all_products.find((e)=>e.id===Number(productId))
   return (
     <div>
-      
+      <BreadCrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
