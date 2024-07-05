07be1bb153cbd21e54b550bfa9e22853d75284d7
hemanthini02 <hemanthinivlr@gmail.com>
2024-07-06 00:44:47 +0530

    created home and furniture page

--- frontend/src/Pages/Fashion/Fashioncategory.jsx
+++ frontend/src/Pages/Fashion/Fashioncategory.jsx
@@ -1,15 +1,41 @@
-// Fashioncategory.js
-import React, { useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import "./ShopCategory.css";
 import { HomeContext } from "../../Context/HomeContext";
 import Item from "../../components/HomeComp/Item/item";
 
 const Fashioncategory = (props) => {
   const { all_products } = useContext(HomeContext);
+  const [magnifierStyle, setMagnifierStyle] = useState({});
+  const [magnifiedImage, setMagnifiedImage] = useState(null);
+  const magnifierRef = useRef();
+
+  const handleMouseEnter = (e, image) => {
+    setMagnifiedImage(image);
+  };
+
+  const handleMouseMove = (e) => {
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+    const x = e.pageX - left - window.pageXOffset;
+    const y = e.pageY - top - window.pageYOffset;
+    const zoomLevel = 2;
+
+    setMagnifierStyle({
+      backgroundImage: `url(${magnifiedImage})`,
+      backgroundSize: `${width * zoomLevel}px ${height * zoomLevel}px`,
+      backgroundPosition: `-${x * (zoomLevel - 1)}px -${y * (zoomLevel - 1)}px`,
+      left: `${e.pageX - 50}px`,
+      top: `${e.pageY - 50}px`,
+      display: 'block'
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setMagnifierStyle({ display: 'none' });
+  };
 
   return (
     <div className='home-category'>
-      <img src={props.banner} alt='Banner' id='banner'/>
+      <img src={props.banner} alt='Banner' id='banner' />
       <div className="fashioncategory-indexsort">
         <p>
           <span>Showing products</span>
@@ -21,7 +47,20 @@ const Fashioncategory = (props) => {
       <div className="fashioncategory-products">
         {all_products.map((item, i) => {
           if (props.category === item.category) {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+            return (
+              <div key={i}>
+                <Item 
+                  id={item.id} 
+                  name={item.name} 
+                  image={item.image} 
+                  new_price={item.new_price} 
+                  old_price={item.old_price} 
+                  onMouseEnter={(e) => handleMouseEnter(e, item.image)}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                />
+              </div>
+            );
           } else {
             return null;
           }
@@ -30,6 +69,7 @@ const Fashioncategory = (props) => {
       <div className="fashioncategory-loadmore">
         Explore more
       </div>
+      <div ref={magnifierRef} className="magnifier" style={magnifierStyle}></div>
     </div>
   );
 }
--- frontend/src/components/HomeComp/Item/item.jsx
+++ frontend/src/components/HomeComp/Item/item.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from 'react';
 import "./Item.css";
 
-const Item = (props) => {
+const Item = ({ id, name, image, new_price, old_price, onMouseEnter, onMouseMove, onMouseLeave }) => {
   return (
-    <div>
-      <img src={props.image} alt=''></img>
-      <p>{props.name}</p>
+    <div className="item">
+      <img 
+        src={image} 
+        alt={name} 
+        onMouseEnter={onMouseEnter} 
+        onMouseMove={onMouseMove} 
+        onMouseLeave={onMouseLeave}
+      />
+      <p>{name}</p>
       <div className='item-prices'>
         <div className="item-price-new">
-            {props.new_price}
+          {new_price}
         </div>
         <div className="item-price-old">
-            {props.old_price}
+          {old_price}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
+
