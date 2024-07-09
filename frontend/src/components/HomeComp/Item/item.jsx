707a8c971bf1f9d14113c0973d57999d1aefd008
hemanthini02 <hemanthinivlr@gmail.com>
2024-07-09 13:38:31 +0530

    changed styling of products

--- frontend/src/components/HomeComp/Item/item.jsx
+++ frontend/src/components/HomeComp/Item/item.jsx
@@ -4,6 +4,7 @@ import "./Item.css";
 const Item = ({ id, name, image, new_price, old_price, onMouseEnter, onMouseMove, onMouseLeave }) => {
   return (
     <div className="item">
+<<<<<<< Updated upstream
       <img 
         src={image} 
         alt={name} 
@@ -12,6 +13,10 @@ const Item = ({ id, name, image, new_price, old_price, onMouseEnter, onMouseMove
         onMouseLeave={onMouseLeave}
       />
       <p>{name}</p>
+=======
+      <Link to={`/product/${props.id}`}><img src={props.image} alt={props.name} /></Link>
+      <p>{props.name}</p>
+>>>>>>> Stashed changes
       <div className='item-prices'>
         <div className="item-price-new">
           {new_price}
@@ -25,4 +30,5 @@ const Item = ({ id, name, image, new_price, old_price, onMouseEnter, onMouseMove
 };
 
 export default Item;
+
 
