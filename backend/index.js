f132e29c7cfe4856c307e0d495b5b6b83ec7a02f
aarshjain09 <jainaarsh1234@gmail.com>
2024-07-19 11:17:20 +0530

    resolve error

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const PORT = 18000;
+const PORT = 10000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 // const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { type } = require("os");
+const { request } = require("http");
+const { error } = require("console");
 
 
 app.use(express.json()); //response will auto pass thorugh json
@@ -16,9 +19,77 @@ app.use(
           origin : "http://localhost:3000"
       })
 )
+const Users=mongoose.model('Users',{
+      name:{
+            type:String
+      },
+      password:{
+            type:String
+      },
+      email:{
+            type:String
+      },
+      cartData:{
+            type:Object
+      },
+      date:{
+            type:Date
+      }
+
+})
+app.post('/signup',async (req,res)=>{
+      let check=await Users.findOne({email:req.body.email});
+      if(check){
+            return res.status(400).json({success:false,errors:"existing users found with same username"})
+      }
+      let cart ={};
+      for(let i=0;i<300;i++){
+            cart[i]=0
+      }
+      const user=new Users({
+      name:req.body.username,
+      email:req.body.email,
+      password:req.body.password,
+      cartData:cart,
+})
+await user.save();
+const data={
+      user:{
+            id:user.id
+      }
+}
+const token=jwt.sign(data,'secret-ecom');
+res.json({success:true,token})
+})
+
+
+app.post('/login',async (req,res)=>{
+let user=await Users.findOne({email:req.body.email});
+if(user){
+      const passCompare=req.body.password===user.password;
+      if(passCompare){
+            const data={
+                  user :{
+                        id:user.id
+                  }
+            }
+            const token=jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+      }
+      else{
+            res.json({success:false,errors:"Wrong password"})
+      }
+}
+else{
+      res.json({success:false,errors:"Wrong email id"})
+}
+
+})
+
 
 //Database connectio with mongo DB
-mongoose.connect("mongodb+srv://shopezdev:eIuRLShLGvtFd08E@cluster0.aml2wwr.mongodb.net/Shop-EZ/Login")
+mongoose.connect("mongodb+srv://shopezdev:eIuRLShLGvtFd08E@cluster0.aml2wwr.mongodb.net/Shop-EZ/Login"
+)
 
 
 
